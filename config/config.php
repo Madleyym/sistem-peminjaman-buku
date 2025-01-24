52b7d72f6f5c1edcd9212e0323babb2da97257b6
Madleyym <tryandaasu@gmail.com>
2025-01-24 12:17:19 +0700

    config mysql

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root"; // Sesuaikan dengan username database Anda
+$password = ""; // Sesuaikan dengan password database Anda
+$dbname = "peminjaman_buku";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
