8785f6cc3bbb2d4522004cfdc1b1d33d51f4b073
Madleyym <tryandaasu@gmail.com>
2025-01-24 12:18:42 +0700

    Query SQL for added users

---
+++ public/register.php
@@ -0,0 +1,19 @@
+<?php
+include "../config/config.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $query = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sss", $name, $email, $password);
+
+    if ($stmt->execute()) {
+        echo "Pendaftaran berhasil!";
+    } else {
+        echo "Pendaftaran gagal: " . $conn->error;
+    }
+}
+?>
